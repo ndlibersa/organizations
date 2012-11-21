3d931c577fda8367c58d7b2dd4c44d551c2ffabf
Robin Schaaf <robinmschaaf@gmail.com>
2012-11-21 12:29:54 -0500

    fix directories in javascript image preloader for menu images

--- js/common.js
+++ js/common.js
@@ -90,7 +90,7 @@ $(function(){
 
 
 
-	jQuery.preLoadImages("images/menu-home-over.gif", "images/menu-neworganization-over.gif", "images/menu-admin-over.gif", "images/menu-end-over.gif");
+	jQuery.preLoadImages("images/menu/menu-home-over.gif", "images/menu/menu-neworganization-over.gif", "images/menu/menu-admin-over.gif", "images/menu/menu-end-over.gif");
 	 
 	 //for swapping menu images
 	$('.rollover').hover(function() {
