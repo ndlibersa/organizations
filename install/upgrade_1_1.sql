b6137951fefcf2f0fd1175a052e3007209752ddc
Cesar HERNANDEZ ANTONIO <hernandez.antonio.cesar19@gmail.com>
2015-06-01 15:47:24 +0200

    Deleted '_DATABASE_NAME_' in the update script (v1.1)

--- install/upgrade_1_1.sql
+++ install/upgrade_1_1.sql
@@ -1,26 +1,15 @@
-
-
-
-
-
-ALTER TABLE `_DATABASE_NAME_`.`Contact` 
+ALTER TABLE `Contact` 
 ADD COLUMN `addressText` varchar(300) default NULL AFTER `title`;
 
-
-UPDATE `_DATABASE_NAME_`.`Contact` SET addressText = CONCAT(state, ", ", country)
+UPDATE `Contact` SET addressText = CONCAT(state, ", ", country)
 WHERE state <> '' AND country <> '';
 
-UPDATE `_DATABASE_NAME_`.`Contact` SET addressText = CONCAT(state, country)
+UPDATE `Contact` SET addressText = CONCAT(state, country)
 WHERE ((state <> '') OR (country <>'')) AND addressText IS NULL;
 
-
-ALTER TABLE `_DATABASE_NAME_`.`Contact`
+ALTER TABLE `Contact`
 DROP COLUMN state,
 DROP COLUMN country;
 
-
-
-ALTER TABLE `_DATABASE_NAME_`.`User` 
+ALTER TABLE `User` 
 ADD COLUMN `accountTabIndicator` int(1) unsigned default '0' AFTER `lastName`;
-
-
