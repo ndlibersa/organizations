9a61ae7d0c513bca369d6431a37bd014f6782b9b
jeremythuff <jeremythuff@gmail.com>
2015-08-18 14:15:00 -0500

    added form validation to create downitme

--- ajax_forms.php
+++ ajax_forms.php
@@ -722,14 +722,14 @@ switch ($_GET['action']) {
 			<td><label>Downtime Start:</label></td>
 			<td>
 				<input value="<?php echo $defaultStart; ?>" type="datetime-local" name="startDate" id="startDate" />
-				<span id='span_error_contactName' class='smallDarkRedText'>
+				<span id='span_error_startDate' class='smallDarkRedText addDowntimeError'></span>
 			</td>
 		</tr>
 		<tr>
 			<td><label>Downtime Resolution:</label></td>
 			<td>
 				<input value="<?php echo $defaultEnd; ?>"  type="datetime-local" name="endDate" id="endDate" />
-				<span id='span_error_contactName' class='smallDarkRedText'>
+				<span id='span_error_endDate' class='smallDarkRedText addDowntimeError'></span>
 			</td>
 		</tr>
 		<tr>
