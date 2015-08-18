70d3dd23e9628b46ed029ad61b021021e34000c1
jeremythuff <jeremythuff@gmail.com>
2015-08-18 15:07:25 -0500

    added validate email funciton to common js

--- js/common.js
+++ js/common.js
@@ -131,7 +131,10 @@ $(function(){
 
 
 
-
+function validateEmail(email) {
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email);
+}
 
 // 1 visible, 0 hidden
 function toggleDivState(divID, intDisplay) {
